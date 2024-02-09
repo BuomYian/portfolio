e9356770d792b33ebe36b5ef604ee7c116909fa3
Buomkuoth Yian <buom.nyajal@gmail.com>
2024-02-09 15:44:50 +0300

    Home page

---
+++ buomkuoth/src/components/home/Social.jsx
@@ -0,0 +1,32 @@
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://github.com/BuomYian"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="uil uil-github-alt"></i>
+      </a>
+      <a
+        href="https://www.instagram.com/buomyian/"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="uil uil-instagram"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/buomkuoth-makuach-3ab137200"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="uil uil-linkedin-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
