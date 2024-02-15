cc04b9cd82eafc391cd4d2a087ec8e322c3df056
Buomkuoth Yian <buom.nyajal@gmail.com>
2024-02-15 18:36:33 +0300

    Done with footer

---
+++ buomkuoth/src/components/footer/Footer.jsx
@@ -0,0 +1,63 @@
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__container container">
+        <h1 className="footer__title">Mal-Tech</h1>
+
+        <ul className="footer__list">
+          <li>
+            <a href="#about" className="footer__link">
+              About
+            </a>
+          </li>
+
+          <li>
+            <a href="#portfolio" className="footer__link">
+              Projects
+            </a>
+          </li>
+
+          <li>
+            <a href="#Testimonials" className="footer__link">
+              Testimonials
+            </a>
+          </li>
+        </ul>
+
+        <div className="footer__social">
+          <a
+            href="https://github.com/BuomYian"
+            className="footer__social-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="uil uil-github-alt"></i>
+          </a>
+          <a
+            href="https://www.instagram.com/buomyian/"
+            className="footer__social-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="bx bxl-instagram"></i>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/buomkuoth-makuach-3ab137200"
+            className="footer__social-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="uil uil-linkedin-alt"></i>
+          </a>
+        </div>
+        <span className="footer__copy">
+          &copy; Mal-Tech. All rights reserved
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
